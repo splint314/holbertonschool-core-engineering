c966c405c4b4d91a095455e60221a68a3f1baeae
splint314 <krigal323@gmail.com>
2026-05-05 08:48:34 +0000

    feat: taking random numbers and telling if they're positive or negative

---
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = __import__('random').randint(-10, 10)
+
+if number > 0:
+    print(f"{number} is positive")
+elif number < 0:
+    print(f"{number} is negative")
+else:
+    print(f"{number} is zero")
