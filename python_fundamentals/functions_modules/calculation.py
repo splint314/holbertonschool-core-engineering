1766d2f47a61b6c48cd5cdcd19c02b4ea310e17e
splint314 <krigal323@gmail.com>
2026-05-07 12:43:23 +0000

    feat: imported imputs into a script

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from  calculator import add, sub, mul, div
+
+if __name__ == "__name__":
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
