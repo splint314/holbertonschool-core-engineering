716941fec6b6827808a50ab231d7e60f31cfa0bf
splint314 <krigal323@gmail.com>
2026-05-08 19:03:24 +0200

    feat: replaced an element of a list at a specific position

---
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def replace_in_list(my_list, idx, element):
+
+    """Replaces an element in a list at a specific position.
+
+    Args:
+        my_list (list): The list to modify.
+        idx (int): The index of the element to replace.
+        element: The new element to insert at the specified index.
+
+    Returns:
+    The modified list with the element replaced,
+    or the original list if the index is out of range.
+    """
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
