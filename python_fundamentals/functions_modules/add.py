a4b1df95af8556e5624cc996957fb7d32e3d6933
splint314 <krigal323@gmail.com>
2026-05-08 21:07:19 +0200

    fix: pep8 coding style

--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -3,10 +3,10 @@
 from add_0 import add
 
 if __name__ == "__main__":
-    
+
     a = 1
     b = 2
-    
+
     result = add(a, b)
-    
+
     print("{} + {} = {}".format(a, b, result))
--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,14 +4,14 @@ def pow(a, b):
 
     if b == 0:
         return 1
-    
+
     exponent = b if b > 0 else -b
     result = 1
-    
+
     for _ in range(exponent):
         result *= a
-        
+
     if b < 0:
         return 1 / result
-        
+
     return result
