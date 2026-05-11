1d6ba3596d52fc73cf410b51383f928ee6795b2d
splint314 <krigal323@gmail.com>
2026-05-11 15:23:08 +0200

    feat: prined a given number of elements of a list

---
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -0,0 +1,22 @@
+from itertools import count
+
+
+def safe_print_list(my_list=[], x=0):
+    """Print x elements of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements to print.
+
+    Returns:
+        int: The number of elements printed.
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
