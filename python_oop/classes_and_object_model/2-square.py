714c6fda74090860618db19796453b7716dd9876
splint314 <krigal323@gmail.com>
2026-05-12 16:21:39 +0200

    fix: changed the argumentation

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
+"""Validate the size of the square."""
 
-def size_validation(size):
-    """Validate the size of the square."""
+
+def size(self):
+
+    """Get the size of the square."""
+
+    return self.__size
+
+
+class Square:
+
+    def __init__(self, size=0):
+
+        """check if size is an integer."""
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
+    """check if size is a positive integer."""
 
     if size < 0:
         raise ValueError("size must be >= 0")
