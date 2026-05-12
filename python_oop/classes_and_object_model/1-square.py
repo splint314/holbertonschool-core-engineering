8c7dd6c66a72c16c0a5db2c83315b96426d0e2f5
splint314 <krigal323@gmail.com>
2026-05-12 15:26:13 +0200

    feat: added private size attribute to Square class

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""A class that defines a square."""
+
+
+class Square:
+
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a size."""
+        self.__size = size
