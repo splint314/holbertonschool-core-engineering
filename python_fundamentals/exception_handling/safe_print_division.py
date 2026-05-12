0160a1174e82b6a53df2906a7081a55780b5c08b
splint314 <krigal323@gmail.com>
2026-05-12 11:04:56 +0200

    fix: pycodestyle

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 def safe_print_division(a, b):
-	"""Divides 2 integers and prints the result.
+    """Divides 2 integers and prints the result.
 
-	Args:
-		a (int): The first integer.
-		b (int): The second integer.
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
 
     Returns:
-        The value of the division, otherwise None if the division can't be done.
+        The value of the division, otherwise None if the division can't be done
     """
-	result = None
-	try:
-		result = a / b
-	except ZeroDivisionError:
-		pass
-	finally:
-		print("Inside result: {}".format(result))
-	return result
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+    return result
