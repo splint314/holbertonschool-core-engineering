c98420ba801f118c9ba912b6457be845d966ecf3
splint314 <krigal323@gmail.com>
2026-05-14 16:24:58 +0200

    fix: finally done with this

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Defines a square with size validation."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a validated size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
