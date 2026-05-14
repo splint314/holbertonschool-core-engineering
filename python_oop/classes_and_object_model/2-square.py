67a6156005d63454269245217212a17fa9e97936
splint314 <krigal323@gmail.com>
2026-05-14 16:28:53 +0200

    fix: actually thinking about jumping from a bridge

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""Validate the size of the square."""
+"""Module that defines a Square class."""
 
 
 class Square:
+    """Defines a square with size validation."""
 
     def __init__(self, size=0):
-        """check if size is an integer."""
+        """Initialize the square with a validated size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-        """check if size is a positive integer."""
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
