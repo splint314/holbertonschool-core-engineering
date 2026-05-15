deb2337cfc6b1f8d82e71c30eeec29ea60db6519
splint314 <krigal323@gmail.com>
2026-05-15 17:55:18 +0200

    fix: fixed circle to handle negative radius using absolute value

--- python_oop/abc/shapes.py
+++ python_oop/abc/shapes.py
@@ -28,11 +28,11 @@ class Circle(Shape):
 
     def area(self):
         """Return area of the circle."""
-        return math.pi * (self.radius ** 2)
+        return math.pi * (abs(self.radius) ** 2)
 
     def perimeter(self):
         """Return perimeter (circumference) of the circle."""
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * abs(self.radius)
 
 
 class Rectangle(Shape):
