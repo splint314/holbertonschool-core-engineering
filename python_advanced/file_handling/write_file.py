cff5bbd5d6f28700311365042dfeb8b2f562c2a3
splint314 <krigal323@gmail.com>
2026-05-26 01:21:59 +0200

    fix: write_file return value and text handling

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-
 """Module that contains the write_file function"""
 
 
 def write_file(filename="", text=""):
-
-    """Writes a string to a text file.
+    """Writes a string to a text file and returns the number of characters.
 
     Args:
         filename (str): The name of the file to write to.
         text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        f.write("Holberton School is so cool!\n")
+        return f.write(text)
