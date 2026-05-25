633f3a4ac1a2646d2d9e0c6406a0e6f1e467fc81
splint314 <krigal323@gmail.com>
2026-05-25 21:24:17 +0200

    fix: shortered the code and removed the import

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -2,19 +2,8 @@
 
 """Module that contains the read_file function"""
 
-from fileinput import filename
-
 
 def read_file(filename: str) -> None:
-
-    """Reads a text file and prints it to stdout
-
-    Args:
-        filename (str): The name of the file to read
-    """
-
-    with open(filename, 'r') as f:
-        print(f.read(), end='')
-
-    if __name__ == "__main__":
-        read_file("my_file_0.txt")
+    """Reads a text file and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
